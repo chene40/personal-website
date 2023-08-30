d0fc9cf09e7c2c9c97b41aefb2744736a0f74684
Eric <chene9802@gmail.com>
2023-08-30 16:20:22 -0400

    chore: modularized related packages

---
+++ components/assets.ts
@@ -0,0 +1,49 @@
+const icons: Record<string, string> = {
+  git: "https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png",
+  github:
+    "https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png",
+  gitlab:
+    "https://user-images.githubusercontent.com/25181517/192108376-c675d39b-90f6-4073-bde6-5a9291644657.png",
+  html: "https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png",
+  css: "https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png",
+  sass: "https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png",
+  tailwindcss:
+    "https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png",
+  swagger:
+    "https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png",
+  firebase:
+    "https://user-images.githubusercontent.com/25181517/189716855-2c69ca7a-5149-4647-936d-780610911353.png",
+  figma:
+    "https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png",
+  javascript:
+    "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
+  react:
+    "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+  typescript:
+    "https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png",
+  npm: "https://user-images.githubusercontent.com/25181517/121401671-49102800-c959-11eb-9f6f-74d49a5e1774.png",
+  yarn: "https://user-images.githubusercontent.com/25181517/183049794-a3dfaddd-22ee-4ffe-b0b4-549ccd4879f9.png",
+  node: "https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png",
+  express:
+    "https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png",
+  redux:
+    "https://user-images.githubusercontent.com/25181517/187896150-cc1dcb12-d490-445c-8e4d-1275cd2388d6.png",
+  jest: "https://user-images.githubusercontent.com/25181517/187955005-f4ca6f1a-e727-497b-b81b-93fb9726268e.png",
+  next: "https://github.com/marwin1991/profile-technology-icons/assets/136815194/5f8c622c-c217-4649-b0a9-7e0ee24bd704",
+  java: "https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png",
+  c: "https://user-images.githubusercontent.com/25181517/192106070-46255bcf-65e6-4c6b-a296-bf8d0d8fb2a7.png",
+  python:
+    "https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png",
+  go: "https://user-images.githubusercontent.com/25181517/192149581-88194d20-1a37-4be8-8801-5dc0017ffbbe.png",
+  postgresql:
+    "https://user-images.githubusercontent.com/25181517/117208740-bfb78400-adf5-11eb-97bb-09072b6bedfc.png",
+  mongodb:
+    "https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png",
+  bash: "https://user-images.githubusercontent.com/25181517/192158606-7c2ef6bd-6e04-47cf-b5bc-da2797cb5bda.png",
+  discord: "https://www.svgrepo.com/show/353655/discord-icon.svg",
+  googlemaps:
+    "https://ww1.freelogovectors.net/wp-content/uploads/2023/04/googlemaps-logo-freelogovectors.net_.png",
+  vercel: "https://bitly.ws/TwAe",
+};
+
+export default icons;
--- components/carousel.tsx
+++ components/carousel.tsx
@@ -3,6 +3,8 @@
 import CarouselItem from "./carousel-item";
 import { useEffect, useState } from "react";
 
+import items from "@/data/carousel-projects";
+
 import Image from "next/image";
 
 export default function Carousel() {
@@ -13,75 +15,6 @@ export default function Carousel() {
     else if (newIndex > items.length - 1) setCurrentIndex(items.length - 1);
     else setCurrentIndex(newIndex);
   };
-
-  const items = [
-    {
-      title: "Poolber",
-      imageSrc: "/PoolberIcon.svg",
-      imageAlt: "Poolber Icon",
-      description: `A user-oriented carpooling app that encourages riders to share riders together. This not only help reduce 
-		the gaseous emissions and traffic congestions, but it also helps you cut down the cost of your ride while 
-		also meeting new people! This app uses the Google Maps API in order to set up the trip route and calculate ride 
-		information such as the order of pickup, ride fare, distance, time, etc.`,
-      techStack: [
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "react",
-        "node",
-        "firebase",
-        "figma",
-        "googlemaps",
-      ],
-      projectLink: "https://github.com/chene40/Poolber/tree/main",
-      githubLink:
-        "https://github.com/chene40/Poolber/tree/main/D4/CarpoolProjectExpo",
-    },
-    {
-      title: "Azeliore Bot",
-      imageSrc: "/GenshinIcon.svg",
-      imageAlt: "Genshin Impact Icon",
-      description: `A discord bot to simulate the Genshin Impact banner system backed with statistical models so users can pull
-	  for free and test their luck. The banners available to wish upon are those on the second phase of version 3.3 (Raiden 
-		Shogun and Ayato's rerun banner). This project was built mostly using Node.js and made use of the discord.js API and
-		a fan-made genshin.js API. Personal information such as pity rates for each banner and account are stored in a MongoDB cluster.`,
-      techStack: ["javascript", "node", "bash", "mongodb", "discord"],
-      projectLink: "https://github.com/chene40/azeliore-bot",
-      githubLink: "https://github.com/chene40/azeliore-bot",
-    },
-    {
-      title: "Tenzies",
-      imageSrc: "/Dice.svg",
-      imageAlt: "An image of a dice",
-      description: `A recreation of the classical game Tenzies. Roll until all the dice are the same and click each die to freeze
-	  its current value between rolls. All relevant data pertaining to the game are stored via local storage and you can check if 
-	  you've beaten your old record. Give it a try and see how fast you can make all 10 dies have the same face!`,
-      techStack: ["html", "css", "javascript", "react", "vercel"],
-      projectLink: "https://tenzies-chene40-mcmasterca.vercel.app/",
-      githubLink: "https://github.com/chene40/Tenzies",
-    },
-    {
-      title: "Personal Website",
-      imageSrc: "/WebsiteIcon.svg",
-      imageAlt: "An image of a dice",
-      description: `The website that you're currently looking at right now is also a personal project that I've created to get to 
-	  know about me a little more. In here, you will find my work, my experiences, and some of my interests. Feel free to take a browse
-	  around and see if anything piques your interest!`,
-      techStack: [
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "react",
-        "next",
-        "tailwindcss",
-        "vercel",
-      ],
-      projectLink: "https://tenzies-chene40-mcmasterca.vercel.app/",
-      githubLink: "https://github.com/chene40/Tenzies",
-    },
-  ];
 
   const autoTransition = () => {
     let nextIndex = currentIndex + 1;
