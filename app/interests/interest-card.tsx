07c25f82f9806279ba5d2481986327853f0d27e5
Eric Chen <chene9802@gmail.com>
2023-09-07 14:23:21 -0400

    fix: fix styling for interests card/page on smaller screen

--- app/interests/interest-card.tsx
+++ app/interests/interest-card.tsx
@@ -11,11 +11,11 @@ export default function InterestCard({ interestData }: InterestCardProps) {
 
   return (
     <div className="bg-red-200 flex flex-col justify-between w-2/5 h-full rounded-3xl">
-      <div className="bg-emerald-100 h-1/6 flex rounded-t-3xl items-center px-4">
+      <div className="bg-emerald-100 h-12 flex rounded-t-3xl items-center px-4">
         <h3>{interest}</h3>
       </div>
-      <div className="flex justify-between h-2/3 p-4">
-        <p className="whitespace-normal w-2/3">{description}</p>
+      <div className="flex justify-between items-center h-2/3 p-4">
+        <p className="whitespace-normal w-2/3 mr-4">{description}</p>
         <div className="h-full w-1/3 flex justify-center">
           <Image
             className="rounded-xl"
@@ -26,7 +26,7 @@ export default function InterestCard({ interestData }: InterestCardProps) {
           />
         </div>
       </div>
-      <button className="bg-blue-100 h-1/6 rounded-b-3xl">
+      <button className="bg-blue-100 h-12 rounded-b-3xl">
         {/* Redirect to the next page on the website e.g. /interests/interestA */}
         <Link href={redirectPath}>Learn More</Link>
       </button>
--- app/interests/page.tsx
+++ app/interests/page.tsx
@@ -8,11 +8,11 @@ export default function Interests() {
   return (
     <div className="h-screen">
       <Header />
-      <div className="bg-gray-900 w-full h-full overflow-hidden">
+      <div className="bg-gray-900 w-full overflow-hidden">
         <h2 className="p-8 text-3xl flex justify-center items-center text-white font-medium">
           Check Out Some of My Interests Below!
         </h2>
-        <div className="h-full flex flex-col">
+        <div className="flex flex-col">
           <div className="flex justify-evenly h-2/5 items-center mb-8 flex-wrap gap-y-12">
             {interests.map((interest, index) => {
               return <InterestCard interestData={interest} key={index} />;
