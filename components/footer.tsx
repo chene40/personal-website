85871a9f8577a22bc5717a7e1d0d39e25b4ec7f2
Eric Chen <chene9802@gmail.com>
2023-09-08 15:22:41 -0400

    fix: reduced space between contact methods

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,14 +9,14 @@ export default function Footer() {
   return (
     <div className="bg-cyan-500 text-white">
       <div className="flex flex-col items-center">
-        <ul className="p-8 w-4/5 flex justify-between">
+        <ul className="p-8 w-4/5 flex justify-between gap-x-6">
           {contactInfo.map((contact, index) => {
             const { socialName, socialLink, imageData } = contact;
             const { url, alt } = imageData;
             return (
               <li
                 key={index}
-                className="w-1/5 rounded-xl bg-cyan-600 active:bg-cyan-700"
+                className="w-full rounded-xl bg-cyan-600 active:bg-cyan-700"
               >
                 <Link
                   href={socialLink}
