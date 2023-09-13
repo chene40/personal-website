4445b3fe54b9debe1606b2a419b7357f87ab86b8
Eric <chene9802@gmail.com>
2023-09-13 00:00:34 -0400

    feat: added computer interest data

---
+++ data/computer.ts
@@ -0,0 +1,30 @@
+const computers = [
+  {
+    cpu: "Intel Core i5-12600K 3.7 GHz 10-Core Processor",
+    cpuCooler: "NZXT Kraken X73 73.11 CFM Liquid CPU Cooler",
+    motherboard: "MSI PRO B660-A DDR4 ATX LGA1700 Motherboard",
+    memory: "Corsair Vengeance RGB Pro 16 GB (2x8 GB) DDR4-3600 CL18 Memory",
+    storage:
+      "Western Digital Blue SN570 1 TB M.2-2280 PCIe 3.0 X4 NVME Solid State Drive",
+    gpu: "MSI GeForce RTX 3060 Ventus 2X 12G GeForce RTX 3060 12GB 12 GB Video Card",
+    case: "Phanteks Eclipse G360A ATX Mid Tower Case",
+    psu: "Thermaltake Smart 700 W 80+ Certified ATX Power Supply",
+    partsList: "https://ca.pcpartpicker.com/user/Yericc98/saved/#view=DR7t3C",
+    blogPost: "https://ca.pcpartpicker.com/b/gyZZxr",
+  },
+  {
+    cpu: "AMD Ryzen 5 5600 3.5 GHz 6-Core Processor",
+    cpuCooler: "Stock Cooler",
+    motherboard: "MSI MAG B550 TOMAHAWK MAX WIFI ATX AM4 Motherboard",
+    memory: "	Corsair Vengeance RGB Pro 16 GB (2x8 GB) DDR4-3200 CL16 Memory",
+    storage:
+      "Western Digital Blue SN570 1 TB M.2-2280 PCIe 3.0 X4 NVME Solid State Drive",
+    gpu: "MSI MECH 2X OC Radeon RX 6650 XT 8 GB Video Card",
+    case: "NZXT H5 Flow RGB ATX Mid Tower Case",
+    psu: "Thermaltake Smart BX1 650 650 W 80+ Bronze Certified ATX Power Supply",
+    partsList: "https://ca.pcpartpicker.com/user/Yericc98/saved/#view=q3DsK8",
+    blogPost: "https://ca.pcpartpicker.com/b/d2ZZxr",
+  },
+];
+
+export default computers;
