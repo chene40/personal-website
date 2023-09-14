488332bb9f89d466322302b70de6efd5126b97dc
Eric <chene9802@gmail.com>
2023-09-13 22:17:10 -0400

    feat: added under development page

---
+++ app/interests/photography/page.tsx
@@ -0,0 +1,17 @@
+// Project Imports
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function Photography() {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="bg-gray-900 grow flex justify-center items-center">
+        <h1 className="text-4xl text-white">
+          This page is currently under development. Please come back later :)
+        </h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
