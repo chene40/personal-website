02b17d5b54e5c42dad69ee1a131654a59922ef92
Eric <chene9802@gmail.com>
2023-09-18 20:00:57 -0400

    feat: added mobile responsiveness to remaining interests page

--- app/interests/alternatives/page.tsx
+++ app/interests/alternatives/page.tsx
@@ -7,8 +7,9 @@ export default function Alternatives() {
     <div className="h-screen flex flex-col">
       <Header />
       <div className="bg-gray-900 grow flex justify-center items-center">
-        <h1 className="text-4xl text-white">
-          This page is currently under development. Please come back later :)
+        <h1 className="flex flex-col gap-y-8 w-10/12 text-4xl text-white text-center">
+          <p>This page is currently under development.</p>
+          <p>Please come back later :)</p>
         </h1>
       </div>
       <Footer />
--- app/interests/photography/page.tsx
+++ app/interests/photography/page.tsx
@@ -2,13 +2,14 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-export default function Photography() {
+export default function Alternatives() {
   return (
     <div className="h-screen flex flex-col">
       <Header />
       <div className="bg-gray-900 grow flex justify-center items-center">
-        <h1 className="text-4xl text-white">
-          This page is currently under development. Please come back later :)
+        <h1 className="flex flex-col gap-y-8 w-10/12 text-4xl text-white text-center">
+          <p>This page is currently under development.</p>
+          <p>Please come back later :)</p>
         </h1>
       </div>
       <Footer />
